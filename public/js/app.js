006df931c1bd3c5b9ee4e25a16e15032d024ef45
Meezyr <meezyr@meezyr.fr>
2023-01-15 17:52:33 +0100

    Front - Sidebar

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
