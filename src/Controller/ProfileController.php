47deb422f3ac1d6887ff8f1650d8e5bd4f1684df
Meezyr <meezyr@meezyr.fr>
2023-01-15 18:08:05 +0100

    Front - Sidebar : Modif liens

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,7 +12,7 @@ class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+
         ]);
     }
 }
