f3b2554bfb22a5a6e697f3079613d55c2480f09f
Meezyr <meezyr@meezyr.fr>
2023-03-04 15:10:22 +0100

    Front : Formulaire Inscription/Connexion

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Gregwar\CaptchaBundle\Type\CaptchaType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,23 +23,36 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstName')
-            ->add('pseudo')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+            ])
             ->add('gender', ChoiceType::class, [
-                'label' => 'Genre :',
+                'label' => 'Genre',
                 'choices'  => [
                     'Homme' => 'homme',
                     'Femme' => 'femme',
                     'Autre' => 'autre',
                 ],
                 'expanded' => true,
-                'multiple' => false
+                'multiple' => false,
+                'choice_attr' => function ($choice, $key, $value) {
+                    return ['class' => 'form-check-input'];
+                },
             ])
             ->add('birthDate', DateType::class, [
                 'widget' => 'single_text',
+                'label' => 'Date de naissance',
+                'attr' => ['class' => 'form-control'],
             ])
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -53,8 +68,18 @@ class RegistrationFormType extends AbstractType
                 'invalid_message' => 'Entrez des mots de passe valides',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => false, 'attr' => ['placeholder' => 'Mot de passe']],
-                'second_options' => ['label' => false, 'attr' => ['placeholder' => 'Confirmer le mot de passe']],
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['placeholder' => 'Mot de passe', 'class' => 'form-control'],
+                    'label_attr' => ['class' => 'form-label'],
+                    'row_attr' => ['class' => 'col-md-6'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => ['placeholder' => 'Confirmer le mot de passe', 'class' => 'form-control'],
+                    'label_attr' => ['class' => 'form-label'],
+                    'row_attr' => ['class' => 'col-md-6'],
+                ],
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
