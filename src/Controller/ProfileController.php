c1f77657e5f2830fe0eeb886589e38c38c81568b
Meezyr <meezyr@meezyr.fr>
2023-03-31 21:28:10 +0200

    Back > Page Profil : Modification infos user

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileFormType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profil', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, UserRepository $userRepository): Response
     {
-        return $this->render('profile/index.html.twig', [
+        $user = $userRepository->find($this->getUser());
 
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('avatar')->getData();
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('avatar_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    return $this->redirectToRoute('app_profile');
+                }
+
+                $user->setAvatar($newFilename);
+            }
+
+            $entityManager->flush();
+        }
+
+        return $this->render('profile/profile.html.twig', [
+            'userForm' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Nom', 'class' => 'form-control'],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Prénom', 'class' => 'form-control'],
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr' => ['placeholder' => 'Pseudo', 'class' => 'form-control'],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['placeholder' => 'Inscrivez votre description ici', 'class' => 'form-control', 'style' => 'height: 100px;'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'attr' => ['placeholder' => 'Adresse e-mail', 'class' => 'form-control'],
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Genre',
+                'choices'  => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme',
+                    'Autre' => 'autre',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
+            ->add('birthDate', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de naissance',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'input-group-text'],
+            ])
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar',
+                'label_attr' => ['class' => 'input-group-text'],
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/svg+xml',
+                            'image/webp',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Envoyer un avatar à un des formats suivant : jpg, png, svg, webp ou gif',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
