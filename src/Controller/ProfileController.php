930e85331daa821fa015ad0abe2137c4df1d1f95
Meezyr <meezyr@meezyr.fr>
2023-03-31 22:13:29 +0200

    Back > Page Profil : Avatar

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
