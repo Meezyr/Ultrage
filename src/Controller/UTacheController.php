d3353b0dda54fcc10d231f0baee509d8fd1e4d48
Meezyr <meezyr@meezyr.fr>
2023-04-05 15:42:16 +0200

    Back > UTâche : Create new task

--- src/Controller/UTacheController.php
+++ src/Controller/UTacheController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,15 +15,53 @@ class UTacheController extends AbstractController
     #[Route('/u-tache', name: 'app_utache')]
     public function index(): Response
     {
-
         $statusbar = [
             'links' => [
-
+                ['title' => 'Tableau des tâches', 'url' => $this->generateUrl("app_utache"), 'target' => false],
             ],
         ];
 
+        if ($this->getUser() != null) {
+            $statusbar['links'][] = ['title' => 'Créer un tâche', 'url' => $this->generateUrl("app_utache_new"), 'target' => false];
+        }
+
+
+
         return $this->render('utache/utache.html.twig', [
             'dataStatusbar' => $statusbar,
+        ]);
+    }
+
+    #[Route('/u-tache/nouvelle-tache', name: 'app_utache_new')]
+    public function newTask(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->getUser() == null) {
+            return $this->redirectToRoute('app_utache');
+        }
+
+        $task = new Task();
+        $form = $this->createForm(TaskFormType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setReleaseDate(new \DateTime());
+
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_utache');
+        }
+
+        $statusbar = [
+            'links' => [
+                ['title' => 'Tableau des tâches', 'url' => $this->generateUrl("app_utache"), 'target' => false],
+                ['title' => 'Créer un tâche', 'url' => $this->generateUrl("app_utache_new"), 'target' => false],
+            ],
+        ];
+
+        return $this->render('utache/new_task.html.twig', [
+            'dataStatusbar' => $statusbar,
+            'newTaskForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/TaskFormType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Statut',
+                'required' => true,
+                'choices'  => [
+                    'A faire' => 0,
+                    'En attente' => 1,
+                    'En cours' => 2,
+                    'Terminé' => 3,
+                ],
+            ])
+            ->add('state', IntegerType::class, [
+                'label' => 'Pourcentage d\'avancement',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
