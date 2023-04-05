82bc4265e50c385bf517abbd14c258cfb6b1d9b3
Meezyr <meezyr@meezyr.fr>
2023-04-05 16:01:48 +0200

    Back > UTâche : Init all task

--- src/Controller/UTacheController.php
+++ src/Controller/UTacheController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskFormType;
+use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class UTacheController extends AbstractController
 {
     #[Route('/u-tache', name: 'app_utache')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepository): Response
     {
         $statusbar = [
             'links' => [
@@ -25,10 +26,44 @@ class UTacheController extends AbstractController
             $statusbar['links'][] = ['title' => 'Créer un tâche', 'url' => $this->generateUrl("app_utache_new"), 'target' => false];
         }
 
+        $allTask = $taskRepository->findAll();
 
+        $arrayTask = [];
+        foreach ($allTask as $task) {
+            if ($task->getStatus() == 0) {
+                $arrayTask[0][] = [
+                    'title' => $task->getTitle(),
+                    'state' => $task->getState(),
+                    'release' => $task->getReleaseDate(),
+                    'update' => $task->getUpdateDate(),
+                ];
+            } elseif ($task->getStatus() == 1) {
+                $arrayTask[1][] = [
+                    'title' => $task->getTitle(),
+                    'state' => $task->getState(),
+                    'release' => $task->getReleaseDate(),
+                    'update' => $task->getUpdateDate(),
+                ];
+            } elseif ($task->getStatus() == 2) {
+                $arrayTask[2][] = [
+                    'title' => $task->getTitle(),
+                    'state' => $task->getState(),
+                    'release' => $task->getReleaseDate(),
+                    'update' => $task->getUpdateDate(),
+                ];
+            } elseif ($task->getStatus() == 3) {
+                $arrayTask[3][] = [
+                    'title' => $task->getTitle(),
+                    'state' => $task->getState(),
+                    'release' => $task->getReleaseDate(),
+                    'update' => $task->getUpdateDate(),
+                ];
+            }
+        }
 
         return $this->render('utache/utache.html.twig', [
             'dataStatusbar' => $statusbar,
+            'tasks' => $arrayTask,
         ]);
     }
 
