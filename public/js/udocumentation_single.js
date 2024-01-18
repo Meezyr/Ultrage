786d8718c4cb0da00c687aaa811fddc663d8cc53
Meezyr <meezyr@meezyr.fr>
2024-01-18 16:00:10 +0100

    Front > UDocumentation : Copy link chapter

--- public/js/udocumentation_single.js
+++ public/js/udocumentation_single.js
@@ -1,5 +1,35 @@
 $(document).ready(function () {
-    $('.contenu-documentation h2, .contenu-documentation h3').each(function(index) {
+    let activeCopy = false;
+
+    $('.contenu-documentation h2, .contenu-documentation h3').each(function (index) {
         $(this).attr('id', $(this).text());
+    });
+
+    $('.contenu-documentation h2 strong, .contenu-documentation h3 strong').click(function () {
+        const idTitle = $(this).parent().attr('id');
+        const textTitle = $(this).text();
+
+        copyToClipboard(encodeURI(window.location.origin + window.location.pathname + '#' + idTitle));
+
+        if (!activeCopy) {
+            activeCopy = true;
+
+            $('main > .content').append(`
+                <div class="toast toast-notif toast-copy-doc show align-items-center text-bg-secondary border-0 rounded-0 rounded-top" role="alert" aria-live="assertive" aria-atomic="true">
+                  <div class="d-flex">
+                    <div class="toast-body">
+                      Lien du chapitre <em>"${textTitle}"</em> copié ✅
+                    </div>
+                    <button type="button" class="btn-close stretched-link me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                  </div>
+                </div>
+            `);
+
+            setTimeout(function () {
+                activeCopy = false;
+
+                $('main > .content > .toast-copy-doc').remove();
+            }, 5000);
+        }
     });
 });
