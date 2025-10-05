ff96ac99e4323e3a8476f71d1f0e2f116cbc4491
Meezyr <corentin.d@polarion.fr>
2025-10-05 22:19:25 +0200

    [Feature] Accueil controller, add link

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ColorRepository;
 use App\Repository\DocumentationRepository;
+use App\Repository\LinkRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(ColorRepository $colorRepository, DocumentationRepository $documentationRepository): Response
+    public function index(ColorRepository $colorRepository, DocumentationRepository $documentationRepository, LinkRepository $linkRepository): Response
     {
         $colors = $colorRepository->findFourOrderByDate('DESC');
 
         $docs = $documentationRepository->findFourOrderByDate('DESC');
 
+        $links = $linkRepository->findFourOrderByDate('DESC');
+
         return $this->render('accueil/accueil.html.twig', [
             'colors' => $colors,
             'docs' => $docs,
+            'links' => $links,
         ]);
     }
 }
